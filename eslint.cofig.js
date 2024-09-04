369419983852561ae00bf92dd609789022e2e3f1
malik-na <malik2015naeem@gmail.com>
2024-09-04 09:26:55 +0530

    eslint config added

---
+++ eslint.cofig.js
@@ -0,0 +1,24 @@
+// eslint.config.js
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      // Add any custom rules here
+    },
+  },
+];
